bc02736680161cc9b2ebd03896a791d6915619fa
booperlv <siblinggamingplays@gmail.com>
2021-11-01 13:21:07 +0800

    fix: change fold to iterate one by one

--- lua/gomove/fold.lua
+++ lua/gomove/fold.lua
@@ -1,63 +1,64 @@
 local fold = {}
 
--- "I need to go move (distance), from (start), (end).
--- Where do you think I should go knowing what you know?"
+-- I want to move (start_low, start_high) (distance) on the screen.
+-- Can you check each line one by one so we can go down?
 -- :: int:destination_low, int:destination_high, bool:did_find_a_fold
 function fold.Handle(start_low, start_high, distance)
   local going_down = (distance > 0)
   local height = start_high - start_low
 
-  --Normally, You would simply go to these locations
-  local destn_base_low = start_low + distance
-  local destn_base_high = start_high + distance
-
   local utils = require("gomove.utils")
-  local lines_between = utils.range(destn_base_low, destn_base_high)
-
-  --But, we need to check first if there are any lines that have folds on them.
-  local line_of_contact
+  local lines_between = utils.range(start_low, start_high)
   if going_down then
-    -- We will sort lines_between to be ascending (loop from top to bottom)
-    -- Already ascending
-  else
     -- We will sort lines_between to be descending (loop from bottom to top)
     table.sort(lines_between, function(a,b) return a > b end)
-  end
-  --Get the first "line of contact" that is inside a fold
-  for _, line in ipairs(lines_between) do
-    local fold_exists = (vim.fn.foldclosedend(line) ~= -1)
-    if fold_exists then
-      line_of_contact = line
-      break
-    end
-  end
-
-  --If there aren't any lines of contact then just return the base destination
-  if not line_of_contact then
-    return destn_base_low, destn_base_high, false
-  end
-
-  --If there are lines_of_contact,
-  --"normal! j" or "normal! k" past them.
-  local destn_low
-  local line_offset = math.abs(distance) - 1
-  vim.fn.cursor(line_of_contact, 1)
-  if going_down then
-    while (vim.fn.foldclosedend(".") ~= -1) do
-      vim.cmd("normal! j")
-    end
-    destn_low = vim.fn.line(".") + line_offset
   else
-    while (vim.fn.foldclosed(".") ~= -1) do
-      vim.cmd("normal! k")
-    end
-    destn_low = vim.fn.line(".") - height - line_offset
+    -- We will sort lines_between to be ascending (loop from top to bottom)
+    -- Already ascending
   end
-  local destn_high = destn_low+height
+  local did_find_a_fold
+  local line_to_first_contact_fold = lines_between[1]
+
+  --Increment each line one by one, and use normal! j or k when finding a fold
+  local destn_low = line_to_first_contact_fold
+  for _=1, math.abs(distance) do
+    if going_down then
+      destn_low = destn_low + 1
+    else
+      destn_low = destn_low - 1
+    end
+    vim.fn.cursor(destn_low, 1)
+
+    if vim.fn.foldclosed(".") ~= -1 then
+      did_find_a_fold = true
+      while (vim.fn.foldclosed(".") ~= -1) do
+        if going_down then
+          vim.cmd("normal! j")
+          destn_low = vim.fn.line(".")
+        else
+          vim.cmd("normal! k")
+          destn_low = vim.fn.line(".") - height
+        end
+      end
+    end
+  end
+
+  --Add cases to handle invalid values
+  local opts = require('gomove.config').opts
+  if destn_low < 0 then
+    destn_low = 1
+  elseif destn_low + height > vim.fn.line('$') then
+    if opts.move_past_end_of_file then
+      destn_low = destn_low
+    else
+      destn_low = vim.fn.line('$') - height
+    end
+  end
+  local destn_high = destn_low + height
 
   return destn_low, destn_high,
   --Return true if did find a fold, false if otherwise
-  (line_of_contact and true or false)
+  (did_find_a_fold and true or false)
 end
 
 return fold
