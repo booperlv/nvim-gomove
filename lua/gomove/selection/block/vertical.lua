4077a0c82651389333c9de2674ef4985fea64d7f
booperlv <siblinggamingplays@gmail.com>
2021-12-29 16:55:42 +0800

    fix: right-to-left block selection not working properly in block vertical

--- lua/gomove/selection/block/vertical.lua
+++ lua/gomove/selection/block/vertical.lua
@@ -180,8 +180,9 @@ function bv.duplicate(vim_start, vim_end, count)
   local line_end = vim.fn.line(vim_end)
   local height = utils.user_height(line_start, line_end)
 
-  local col_start = vim.fn.col(vim_start)
-  local col_end = vim.fn.col(vim_end)
+  local temp_cols = {vim.fn.col(vim_start), vim.fn.col(vim_end)}
+  local col_start = math.min(unpack(temp_cols))
+  local col_end = math.max(unpack(temp_cols))
   local width = col_end - col_start
 
   if utils.contains_fold(line_start, line_end) then
