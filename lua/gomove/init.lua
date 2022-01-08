5c6d2a4734d96deabe6ad8664817d0a9dd55fcfd
booperlv <siblinggamingplays@gmail.com>
2022-01-08 17:12:47 +0800

    fix: issues with specific mappings due to not using nnoremap on <Plug> mappings

--- lua/gomove/init.lua
+++ lua/gomove/init.lua
@@ -10,25 +10,25 @@ gomove.opts = {
 local function map_defaults()
   local map = require("gomove.mappings").map
   map ({
-    {"n", "<A-h>", "<Plug>GoNSMLeft"},
-    {"n", "<A-j>", "<Plug>GoNSMDown"},
-    {"n", "<A-k>", "<Plug>GoNSMUp"},
-    {"n", "<A-l>", "<Plug>GoNSMRight"},
+    {"n", "<A-h>", "<Plug>GoNSMLeft", {}},
+    {"n", "<A-j>", "<Plug>GoNSMDown", {}},
+    {"n", "<A-k>", "<Plug>GoNSMUp", {}},
+    {"n", "<A-l>", "<Plug>GoNSMRight", {}},
 
-    {"x", "<A-h>", "<Plug>GoVSMLeft"},
-    {"x", "<A-j>", "<Plug>GoVSMDown"},
-    {"x", "<A-k>", "<Plug>GoVSMUp"},
-    {"x", "<A-l>", "<Plug>GoVSMRight"},
+    {"x", "<A-h>", "<Plug>GoVSMLeft", {}},
+    {"x", "<A-j>", "<Plug>GoVSMDown", {}},
+    {"x", "<A-k>", "<Plug>GoVSMUp", {}},
+    {"x", "<A-l>", "<Plug>GoVSMRight", {}},
 
-    {"n", "<A-H>", "<Plug>GoNSDLeft"},
-    {"n", "<A-J>", "<Plug>GoNSDDown"},
-    {"n", "<A-K>", "<Plug>GoNSDUp"},
-    {"n", "<A-L>", "<Plug>GoNSDRight"},
+    {"n", "<A-H>", "<Plug>GoNSDLeft", {}},
+    {"n", "<A-J>", "<Plug>GoNSDDown", {}},
+    {"n", "<A-K>", "<Plug>GoNSDUp", {}},
+    {"n", "<A-L>", "<Plug>GoNSDRight", {}},
 
-    {"x", "<A-H>", "<Plug>GoVSDLeft"},
-    {"x", "<A-J>", "<Plug>GoVSDDown"},
-    {"x", "<A-K>", "<Plug>GoVSDUp"},
-    {"x", "<A-L>", "<Plug>GoVSDRight"},
+    {"x", "<A-H>", "<Plug>GoVSDLeft", {}},
+    {"x", "<A-J>", "<Plug>GoVSDDown", {}},
+    {"x", "<A-K>", "<Plug>GoVSDUp", {}},
+    {"x", "<A-L>", "<Plug>GoVSDRight", {}},
   })
 end
 
--- lua/gomove/mappings/init.lua
+++ lua/gomove/mappings/init.lua
@@ -5,10 +5,10 @@ function M.map(allkeys)
   for index, value in ipairs(allkeys) do
     if type(value) == "table" then
       -- mode, key, value, opts
-      if #value >= 4 then
+      if #value == 4 then
         vim.api.nvim_set_keymap(value[1], value[2], value[3], value[4])
       else
-        vim.api.nvim_set_keymap(value[1], value[2], value[3], {silent=true})
+        vim.api.nvim_set_keymap(value[1], value[2], value[3], {silent=true, noremap=true})
       end
     else
       print("mapping has invalid values at index "..index)
